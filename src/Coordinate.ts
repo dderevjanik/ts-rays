287a634d6380cdb6549f85262d1db44d914d60d9
dderevjanik <daniel.derevjanik@gmail.com>
2016-04-16 19:01:34 +0200

    added comments to Coordinate.ts

--- src/Coordinate.ts
+++ src/Coordinate.ts
@@ -1,16 +1,50 @@
 import {IPoint, ICell} from './Interfaces/all';
+import {staticData} from './StaticData';
 
+type CellToPoint = (row: number, col: number) => IPoint;
+type PointToCell = (x: number, y: number) => ICell;
+
+/**
+ * Get world coordinations
+ * @param {number} scale
+ * @param {number} x
+ * @param {number} y
+ * @retunr {IPoint}
+ */
 const getWPos = (scale: number, row: number, col: number): IPoint => ({
     x: (col * scale),
     y: (row * scale)
 });
 
+/**
+ * Get current cell position
+ * @param {number} scale
+ * @param {number} x
+ * @param {number} y
+ * @retunr {ICell}
+ */
 const getCPos = (scale: number, x: number, y: number): ICell => ({
     row: (Math.floor(y / scale)),
     col: (Math.floor(x / scale))
 });
 
+/**
+ * Get world coordinations
+ * @param {number} row
+ * @param {number} col
+ * @return {IPoint}
+ */
+const getCurrentWPos: CellToPoint = getWPos.bind(this, staticData.ui.minimap.scale);
+
+/**
+ * Get current cell
+ * @param {number} x
+ * @param {number} y
+ * @return {ICell}
+ */
+const getCurrentCPos: PointToCell = getCPos.bind(this, staticData.ui.minimap.scale);
+
 export default {
-    getWPos: getWPos,
-    getCPos: getCPos
+    getWPos: getCurrentWPos,
+    getCPos: getCurrentCPos
 }

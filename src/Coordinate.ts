253a1ca5064953e379e4bf44e911f6e6dc4e78d3
dderevjanik <daniel.derevjanik@gmail.com>
2016-04-24 16:06:24 +0200

    added isCOutside and isWOutside func to Coordinate

--- src/Coordinate.ts
+++ src/Coordinate.ts
@@ -29,12 +29,36 @@ const getCPos = (scale: number, x: number, y: number): ICell => ({
 });
 
 /**
+ * Check if coordinates are outside of world
+ * @param {number} scale
+ * @param {Array<Array<number>>} map
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+const isWOutside = (scale: number, map: Array<Array<number>>, x: number, y: number): boolean =>
+    ((x < 0) || (x >= map[0].length) ||
+    (y < 0) || (y >= map.length));
+
+/**
+ * Check if cell is outside of world
+ * @param {Array<Array<number>>} map
+ * @param {number} row
+ * @param {number} cell
+ * @return {boolean}
+ */
+const isCOutside = (map: Array<Array<number>>, row: number, cell: number): boolean =>
+    ((row < 0) || (row >= map.length) ||
+    (cell < 0) || (cell >= map[0].length));
+
+/**
  * Get world coordinations
  * @param {number} row
  * @param {number} col
  * @return {IPoint}
  */
-const getCurrentWPos: CellToPoint = getWPos.bind(this, staticData.ui.minimap.scale);
+const getCurrentWPos: CellToPoint
+    = getWPos.bind(this, staticData.ui.minimap.scale);
 
 /**
  * Get current cell
@@ -42,9 +66,22 @@ const getCurrentWPos: CellToPoint = getWPos.bind(this, staticData.ui.minimap.sca
  * @param {number} y
  * @return {ICell}
  */
-const getCurrentCPos: PointToCell = getCPos.bind(this, staticData.ui.minimap.scale);
+const getCurrentCPos: PointToCell
+    = getCPos.bind(this, staticData.ui.minimap.scale);
+
+/**
+ * Check if coordinates are outside of world
+ * @param {Array<Array<number>>} map
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+const isCurrentWOutside: boolean
+    = isWOutside.bind(this, staticData.ui.minimap.scale);
 
 export default {
     getWPos: getCurrentWPos,
-    getCPos: getCurrentCPos
-}
+    getCPos: getCurrentCPos,
+    isWOutside: isWOutside,
+    isCOutside: isCOutside
+};
